fff55242da4236f84afd4977bf885b699cf3df66
goldmanleo@mail.ru <goldmanleo@mail.ru>
2021-03-14 18:10:12 +0300

    delete pod

--- SkillBoxLesson4/Pods/Target Support Files/Pods-SkillBoxLesson4-SkillBoxLesson4UITests/Pods-SkillBoxLesson4-SkillBoxLesson4UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SkillBoxLesson4_SkillBoxLesson4UITests : NSObject
-@end
-@implementation PodsDummy_Pods_SkillBoxLesson4_SkillBoxLesson4UITests
-@end
