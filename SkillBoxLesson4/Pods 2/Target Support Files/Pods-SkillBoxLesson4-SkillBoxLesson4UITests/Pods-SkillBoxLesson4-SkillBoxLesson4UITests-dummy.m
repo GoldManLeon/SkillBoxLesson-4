7f243ace2c27c3836808083185bfb7ea20212ef5
goldmanleo@mail.ru <goldmanleo@mail.ru>
2021-03-16 21:59:28 +0300

    Have request

---
+++ SkillBoxLesson4/Pods 2/Target Support Files/Pods-SkillBoxLesson4-SkillBoxLesson4UITests/Pods-SkillBoxLesson4-SkillBoxLesson4UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkillBoxLesson4_SkillBoxLesson4UITests : NSObject
+@end
+@implementation PodsDummy_Pods_SkillBoxLesson4_SkillBoxLesson4UITests
+@end
--- SkillBoxLesson4/SkillBoxLesson4/AViewController/ATestViewController.swift
+++ SkillBoxLesson4/SkillBoxLesson4/AViewController/ATestViewController.swift
@@ -8,64 +8,56 @@ import Alamofire
 class ATestViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     // MARK: - IBOutlet
-    
+
     @IBOutlet weak var tableView: UITableView!
-    
+
     // MARK: - Variable
-    
+
     var nameLabelVariable = ""
     
     // MARK: - Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         func setLabelsForData(){
             AF.request("https://blackstarshop.ru/index.php?route=api/v1/categories").responseJSON{response in
                 if let responseData = response.value as? [String:Any] {
                     if let dict = responseData["67"] as? [String:Any]{
                         if let name = dict["name"] as? String{
                             self.nameLabelVariable = name
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
                         }
                     }
                 }
             }
         }
-        
         setLabelsForData()
-        
-        
     }
-    
+
     // MARK: - TableView
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return nameLabelVariable.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
         cell.label.text = nameLabelVariable
         return cell
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
-    
-    // MARK: - Func download for json
-    
-//    func downloadJson(){
-//        let urlString = "https://blackstarshop.ru/index.php?route=api/v1/categories"
-//            guard let url = URL(string: urlString) else {return}
-//                let task =  URLSession.shared.dataTask(with: url){ data, response, error in
-//                    if let data = data, error == nil{
-//                        DispatchQueue.main.async {
-//                    self.setLabelsForData(for: data as NSData)
-//                }
-//            }
-//        }; task.resume()
-//    }
-    
-    
 
