59644a50e32d68290784f6ddad5662ced231b615
goldmanleo@mail.ru <goldmanleo@mail.ru>
2021-04-05 19:41:47 +0300

    MVVM a

--- SkillBoxLesson4/SkillBoxLesson4/AViewControllers/MVVM/Controller/MVVMAViewController.swift
+++ SkillBoxLesson4/SkillBoxLesson4/AViewControllers/MVVM/Controller/MVVMAViewController.swift
@@ -2,11 +2,14 @@
 import UIKit
 class MVVMAViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
+    // MARK: - IBOutlet
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Variables
     var viewModel: ViewModelDelegate?
     var nameOfViewModel: ViewModel?
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -18,6 +21,7 @@ class MVVMAViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
+    // MARK: - TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel?.numberOfRows() ?? 0
     }
